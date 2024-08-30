779620be73d1570e6a43df1f0f1020d74feb3d8d
kerlynn1 <kerlynn@bookdoc.com>
2024-08-31 01:16:19 +0800

    add prepend_http_if_missing

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,9 @@
 class Url < ApplicationRecord
   has_many :visits, dependent: :destroy
-  validates :target_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  before_validation :prepend_http_if_missing
+
+  validates :target_url, presence: true
   validates :short_url, uniqueness: true, length: { maximum: 15 }
 
   before_create :set_short_url
@@ -10,6 +13,12 @@ class Url < ApplicationRecord
   end
 
   private
+
+  def prepend_http_if_missing
+    return if target_url.blank?
+
+    self.target_url = "http://#{target_url}" unless target_url.match?(%r{\Ahttp://|https://})
+  end
 
   def set_short_url
     UrlShortenerService.new(self).generate_short_url
