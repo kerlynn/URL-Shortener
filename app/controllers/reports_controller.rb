7fa98bef01c42611b51294ece57a81abce7b4a2b
kerlynn1 <kerlynn@bookdoc.com>
2024-09-01 17:33:10 +0800

    update report ui and add target_url, title

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def usage
-    grouped_visits = Visit.order(created_at: :desc).group_by { |visit| visit.url.short_url }
+    grouped_visits = Visit.includes(:url).order(created_at: :desc).group_by { |visit| visit.url }
     @pagy, @grouped_visits = pagy_array(grouped_visits.to_a, limit: 10)
   end
 end
